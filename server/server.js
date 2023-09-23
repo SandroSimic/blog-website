8cc83f2e5c5bcded1a1a58724e5a3cf2060d7bc6
Sandro Simic <sandrosimic8@gmail.com>
2023-09-23 21:46:21 +0200

    Relocated socket index to server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import { Server } from "socket.io";
 import express from "express"
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -43,6 +44,53 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-app.listen(process.env.PORT, (req, res) => {
+const server = app.listen(process.env.PORT, (req, res) => {
   console.log("server running on port " + process.env.PORT)
 })
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173"
+  }
+})
+
+
+let onlineUsers = []
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some(user => user.username === username) && onlineUsers.push({ username, socketId })
+}
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (username) => {
+  return onlineUsers.find(user => user.username === username)
+}
+
+io.on('connection', (socket) => {
+
+  console.log('A user connected')
+
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id)
+  })
+
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    console.log('Received sendNotification event');
+    const receiver = getUser(receiverName);
+    console.log(receiver)
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName, type
+    });
+  });
+
+  socket.on('disconnect', () => {
+    removeUser(socket.id)
+    console.log("user disconnected")
+  })
+})
+
+io.listen(3000)
